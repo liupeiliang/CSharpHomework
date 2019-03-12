43fd9c17c860509cbf729d9c50dd214c23b6a01a
liupeiliang <47717801+liupeiliang@users.noreply.github.com>
2019-03-12 20:02:08 +0800

    Add files via upload

---
+++ Homework3/Factory Method.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CSharpHomework
+{
+	//日志记录器接口：抽象产品  
+	public interface Logger { void WriteLog();  }
+
+	//数据库日志记录器：具体产品  
+	class DatabaseLogger : Logger
+	{
+		public void WriteLog() { Console.WriteLine("数据库日志记录。"); }
+	}
+
+	//文件日志记录器：具体产品  
+	class FileLogger : Logger
+	{
+		public void WriteLog() { Console.WriteLine("文件日志记录。"); }
+	}
+
+	//日志记录器工厂接口：抽象工厂 
+	public interface LoggerFactory
+	{
+		Logger CreateLogger();
+	}
+
+	//数据库日志记录器工厂类：具体工厂  
+	class DatabaseLoggerFactory : LoggerFactory
+	{
+		public Logger CreateLogger()
+		{
+			//连接数据库，代码省略  
+			//创建数据库日志记录器对象  
+			Logger logger = new DatabaseLogger();
+			//初始化数据库日志记录器，代码省略  
+			return logger;
+		}
+	}
+
+	//文件日志记录器工厂类：具体工厂  
+	class FileLoggerFactory : LoggerFactory
+	{
+		public Logger CreateLogger()
+		{
+			//创建文件日志记录器对象  
+			Logger logger = new FileLogger();
+			//创建文件，代码省略  
+			return logger;
+		}
+	}
+
+	class Client
+	{
+		public static void Main(String [] args)
+		{
+			LoggerFactory factory;
+			Logger logger;
+			factory = new FileLoggerFactory(); //可引入配置文件实现  
+			logger = factory.CreateLogger();
+			logger.WriteLog();
+		}
+	}
+
+}
---
+++ Homework3/Simple Factory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CSharpHomework
+{
+    public interface Chart { void display();  }
+
+	class HistogramChart : Chart
+	{
+		public HistogramChart() { Console.WriteLine("Creating HistogramChart!"); }
+		public void display() { Console.WriteLine("Display HistogramChart!"); }
+	}
+
+	class PieChart : Chart
+	{
+		public PieChart() { Console.WriteLine("Creating PieChart!"); }
+		public void display() { Console.WriteLine("Dispaly PieChart!"); }
+	}
+
+	class LineChart : Chart
+	{
+		public LineChart() { Console.WriteLine("Creating LineChart!"); }
+		public void display() { Console.WriteLine("Dispaly LineChart!"); }
+	}
+
+	class ChartFactory
+	{
+		public static Chart getChart(String type)
+		{
+			Chart chart = null;
+			if (type == "histogram")
+			{
+				chart = new HistogramChart();
+				Console.WriteLine("initializing HistogramChart!");
+			}
+			else if (type == "pie")
+			{
+				chart = new PieChart();
+				Console.WriteLine("initializing PieChart!");
+			}
+			else if (type == "line")
+			{
+				chart = new LineChart();
+				Console.WriteLine("initializing LineChart!");
+			}
+			return chart;
+		}
+	}
+
+	class Client
+	{
+		public static void Main(String[] args)
+		{
+			Chart chart;
+			chart = ChartFactory.getChart("histogram");
+			chart.display();
+		}
+	}
+
+}
