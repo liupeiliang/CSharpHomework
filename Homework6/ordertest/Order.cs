ca6f7bd54caf805641d9bdeec4a43e50bd489aa9
liupeiliang <47717801+liupeiliang@users.noreply.github.com>
2019-04-04 22:11:44 +0800

    Add files via upload

---
+++ Homework6/ordertest/Order.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordertest {
+
+    /// <summary>
+    /// Order class : all orderDetails
+    /// to record each goods and its quantity in this ordering
+    /// </summary>
+    class Order:IComparable {
+
+        private List<OrderDetail> details=new List<OrderDetail>();
+
+        /// <summary>
+        /// Order constructor
+        /// </summary>
+        /// <param name="orderId">order id</param>
+        /// <param name="customer">who orders goods</param>
+        public Order(uint orderId, Customer customer) {
+            Id = orderId;
+            Customer = customer;
+        }
+
+        /// <summary>
+        /// order id
+        /// </summary>
+        public uint Id { get; set; }
+
+        /// <summary>
+        /// the man who orders goods
+        /// </summary>
+        public Customer Customer { get; set; }
+
+
+        public List<OrderDetail> Details {
+            get =>this.details; }
+
+		public uint Amount
+		{
+			get
+			{
+				uint sum = 0;
+				Details.ForEach(s => sum += s.Quantity);
+				return sum;
+			}
+		}
+
+		/// <summary>
+		/// add new orderDetail to order
+		/// </summary>
+		/// <param name="orderDetail">the new orderDetail which will be added</param>
+		public void AddDetails(OrderDetail orderDetail) {
+            if (this.Details.Contains(orderDetail))  {
+                throw new Exception($"orderDetails-{orderDetail.Id} is already existed!");
+            }
+            details.Add(orderDetail);
+        }
+
+		public override bool Equals(object obj)
+		{
+			return obj is Order o && o.Id == this.Id && o.Customer == this.Customer;
+		}
+
+		public override int GetHashCode()
+		{
+			var hashCode = 1522631281;
+			hashCode = hashCode * -1521134295 + Id.GetHashCode();
+			hashCode = hashCode * -1521134295 + Customer.GetHashCode();
+			return hashCode;
+		}
+
+		/// <summary>
+		/// remove orderDetail by orderDetailId from order
+		/// </summary>
+		/// <param name="orderDetailId">id of the orderDetail which will be removed</param>
+		public void RemoveDetails(uint orderDetailId) {
+            details.RemoveAll(d =>d.Id==orderDetailId);
+        }
+
+        /// <summary>
+        /// override ToString
+        /// </summary>
+        /// <returns>string:message of the Order object</returns>
+        public override string ToString() {
+            String result = $"orderId:{Id}, customer:({Customer})";
+            details.ForEach(detail => result += "\n\t" + detail);
+            return result;
+        }
+
+		public int CompareTo(object o)
+		{
+			var t = o as Order;
+			if (this.Id > t.Id)
+				return 1;
+			else if (this.Id < t.Id)
+				return -1;
+			else return 0;
+		}
+
+    }
+}
---
+++ Homework6/ordertest/OrderService.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordertest {
+  /// <summary>
+  /// OrderService:provide ordering service,
+  /// like add order, remove order, query order and so on
+  /// 实现添加订单、删除订单、修改订单、查询订单（按照订单号、商品名称、客户等字段进行查询)
+  /// </summary>
+  class OrderService {
+
+    private List<Order> orderList;
+    /// <summary>
+    /// OrderService constructor
+    /// </summary>
+    public OrderService() {
+      orderList = new List<Order>();
+    }
+
+    /// <summary>
+    /// add new order
+    /// </summary>
+    /// <param name="order">the order will be added</param>
+    public void AddOrder(Order order) {
+     //orderList.
+      foreach (Order o in orderList) {
+        if (o.Id.Equals(order.Id)) {
+          throw new Exception($"order-{order.Id} is already existed!");
+        }
+      }
+      orderList.Add(order);
+      //orderList.ForEach(o=> )
+    }
+
+    /// <summary>
+    /// query by orderId
+    /// </summary>
+    /// <param name="orderId">id of the order to find</param>
+    /// <returns>List<Order></returns> 
+    public Order GetById(uint orderId) {
+      foreach (Order o in orderList) {
+        if (o.Id == orderId) {
+          return o;
+        }
+      }
+      return null;
+    }
+
+    /// <summary>
+    /// remove order
+    /// </summary>
+    /// <param name="orderId">id of the order which will be removed</param> 
+    public void RemoveOrder(uint orderId) {
+      Order order = GetById(orderId);
+      if (order == null) return;
+      orderList.Remove(order);
+    }
+
+    /// <summary>
+    /// query all orders
+    /// </summary>
+    /// <returns>List<Order>:all the orders</returns> 
+    public List<Order> QueryAllOrders() {
+      return orderList;
+    }
+
+   
+    /// <summary>
+    /// query by goodsName
+    /// </summary>
+    /// <param name="goodsName">the name of goods in order's orderDetail</param>
+    /// <returns></returns> 
+    public List<Order> QueryByGoodsName(string goodsName) {
+      List<Order> result = new List<Order>();
+      foreach (Order order in orderList) {
+        foreach (OrderDetail detail in order.Details) {
+          if (detail.Goods.Name == goodsName) {
+            result.Add(order);
+            break;
+          }
+        }
+      }
+      return result;
+    }
+
+		public List<Order> QueryByGoodsNameLinq(string goodsName)
+		{
+			List<Order> result = new List<Order>();
+			var query = from i in orderList
+						from j in i.Details
+						where j.Goods.Name == goodsName
+						select i;
+			return query.ToList();
+		}
+
+		/// <summary>
+		/// query by customerName
+		/// </summary>
+		/// <param name="customerName">customer name</param>
+		/// <returns></returns> 
+		public List<Order> QueryByCustomerName(string customerName) {
+			//var query = orderList
+			//	  .Where(order => order.Customer.Name == customerName);
+			// return query.ToList();
+
+			var query = from i in orderList
+						where i.Customer.Name == customerName
+						select i;
+			return query.ToList();
+		}
+
+
+		public void SortById()
+	{
+		orderList.Sort();
+	}
+	
+		public void SortByAmount()
+		{
+			orderList = orderList.OrderByDescending(s => s.Amount).ToList<Order>();
+		}
+
+		public void SortByAmountLinq()
+		{
+			orderList = (from s in orderList orderby s.Amount descending select s).ToList<Order>();
+		}
+
+		// 将所有订单序列化为XML文件
+		public void Export(string filename)
+		{
+			XmlSerializer xmls = new XmlSerializer(orderList.GetType());
+			try
+			{
+				using (FileStream fs = new FileStream(filename, FileMode.Create))
+				{
+					xmls.Serialize(fs, orderList);
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("导出为XML文件时出错。错误信息：" + e);
+			}
+		}
+
+		// 从XML文件中载入订单
+		public void Import(string filename)
+		{
+			XmlSerializer xmls = new XmlSerializer(orderList.GetType());
+			try
+			{
+				using (FileStream fs = new FileStream(filename, FileMode.Open))
+				{
+					orderList = (List<Order>)xmls.Deserialize(fs);
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("从XML文件中载入订单时。出错信息：" + e);
+			}
+		}
+	}
+}
